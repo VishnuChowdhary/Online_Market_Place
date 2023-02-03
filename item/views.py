026a01e2a9d014b7cbf49104af8c9c4f49915232
VishnuChowdhary <112419086+VishnuChowdhary@users.noreply.github.com>
2023-02-03 14:30:53 +0530

    Search and filters are added

--- item/views.py
+++ item/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-from item.models import Item
+from item.models import Item, Category
 from item.forms import NewItemForm, EditItemForm
 
 # Create your views here.
+def items(request):
+    query = request.GET.get('query', '')
+    categories = Category.objects.all()
+    category_id = request.GET.get('category', 0)
+    items = Item.objects.filter(is_sold=False)
+    if category_id:
+        items = items.filter(category_id=category_id)
+    if query:
+        items = items.filter(name__icontains=query)
+    context = {
+        'items': items,
+        'query': query,
+        'categories': categories,
+        'category_id': int(category_id),
+    }
+    return render(request, 'item/items.html', context)
+
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
     related_items = Item.objects.filter(category=item.category, is_sold=False).exclude(pk=pk)[0:3]
